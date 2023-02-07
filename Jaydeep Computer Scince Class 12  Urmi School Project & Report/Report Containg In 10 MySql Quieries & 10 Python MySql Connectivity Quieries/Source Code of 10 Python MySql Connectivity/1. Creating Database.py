13de189cd864d7724c730ccff489a3a65e9c7846
Jaydeep <anij9825@gmail.com>
2023-02-08 00:05:58 +0530

    Delete Jaydeep Computer Scince Class 12  Urmi School Project & Report directory

--- Jaydeep Computer Scince Class 12  Urmi School Project & Report/Report Containg In 10 MySql Quieries & 10 Python MySql Connectivity Quieries/Source Code of 10 Python MySql Connectivity/1. Creating Database.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password=""
-)
-mycursor = mydb.cursor()
-mycursor.execute("CREATE DATABASE mydb_test")
